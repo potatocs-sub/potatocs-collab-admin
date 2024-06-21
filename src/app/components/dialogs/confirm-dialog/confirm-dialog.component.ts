9f2af7a7b894f181040bf3458c1141ad18984d6c
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-06-21 10:42:39 +0900

    [feat] dialog css 수정

--- src/app/components/dialogs/confirm-dialog/confirm-dialog.component.ts
+++ src/app/components/dialogs/confirm-dialog/confirm-dialog.component.ts
@@ -1,18 +1,18 @@
-import { Component, inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { MaterialsModule } from '../../../materials/materials.module';
 import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MaterialsModule } from '../../../materials/materials.module';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-dialog',
   standalone: true,
   imports: [CommonModule, MaterialsModule],
   templateUrl: './confirm-dialog.component.html',
-  styleUrl: './confirm-dialog.component.scss'
+  styleUrl: './confirm-dialog.component.scss',
 })
 export class ConfirmDialogComponent {
-  public dialogRef = inject(MatDialogRef<ConfirmDialogComponent>)
-  public data = inject(MAT_DIALOG_DATA)
+  public dialogRef = inject(MatDialogRef<ConfirmDialogComponent>);
+  public data = inject(MAT_DIALOG_DATA);
 
   ngOnInit(): void {
     this.data.flag = true;
