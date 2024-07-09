305807419f4bed444da13348f9fbaaf5d8194bbf
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-07-09 15:47:57 +0900

    [feat] edit profile 추가

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,19 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
-import { MaterialsModule } from '../../materials/materials.module';
-import { DialogService } from '../../stores/dialog/dialog.service';
+import { Component, inject } from '@angular/core';
 import {
   AbstractControl,
   FormBuilder,
   FormControl,
   FormGroup,
-  ReactiveFormsModule,
   ValidationErrors,
   ValidatorFn,
   Validators,
 } from '@angular/forms';
+import { MaterialsModule } from '../../materials/materials.module';
+import { AuthService } from '../../services/auth/auth.service';
+import { DialogService } from '../../stores/dialog/dialog.service';
+import { Router } from '@angular/router';
 import { ProfileService } from '../../services/profile/profile.service';
-import { confirmPasswordValidator } from './confirm-password.validator';
 
 export const comparePasswordValidator: ValidatorFn = (
   control: AbstractControl
@@ -29,106 +29,76 @@ export const comparePasswordValidator: ValidatorFn = (
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, MaterialsModule, ReactiveFormsModule],
+  imports: [CommonModule, MaterialsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
-  profileService = inject(ProfileService);
+  authService = inject(AuthService);
+  userInfoStore = this.authService.userInfo;
+  editProfileForm: FormGroup;
   dialogService = inject(DialogService);
-  fb = inject(FormBuilder);
+  profileService = inject(ProfileService);
 
-  profile_img: any;
-  email: any;
-  _id: any;
-
-  editProfileForm: FormGroup = this.fb.group(
-    {
-      name: new FormControl('', [Validators.required]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(15),
-      ]),
-      confirmPassword: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(15),
-      ]),
-    },
-    { validators: confirmPasswordValidator }
-  );
-  router: any;
-
-  // isLoadingResults = signal<boolean>(true);
-  // isRateLimitReached = signal<boolean>(false);
-
-  constructor() {
-    this.getProfile();
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.editProfileForm = this.fb.group(
+      {
+        name: new FormControl('', [Validators.required]),
+        password: new FormControl(''),
+        confirmPassword: new FormControl(''),
+      },
+      { validators: comparePasswordValidator }
+    );
   }
 
-  getProfile() {
-    console.log('들어옴');
-    this.profileService.getUserProfile().subscribe((res: any) => {
-      console.log(res);
-
-      this.profile_img = res.profile_img;
-      this.email = res.email;
-      this._id = res._id;
-
-      this.editProfileForm.patchValue({
-        name: res.name,
-      });
-    });
+  ngOnInit() {
+    this.editProfileForm.patchValue(this.userInfoStore());
   }
 
   fileChangeEvent(event: any) {
-    console.log(this.editProfileForm);
     if (event.target.files && event.target.files[0]) {
       if (
         event.target.files[0].name.toLowerCase().endsWith('.jpg') ||
         event.target.files[0].name.toLowerCase().endsWith('.png')
       ) {
-        // Image resize and update
         this.changeProfileImage(event.target.files[0]);
       } else {
         this.dialogService.openDialogNegative(
           'Profile photos are only available for PNG and JPG.'
         );
-        // alert('프로필 사진은 PNG와 JPG만 가능합니다.');
       }
     } else {
       this.dialogService.openDialogNegative('Can not bring up pictures.');
-      // alert('사진을 불러올 수 없습니다.');
     }
   }
 
   changeProfileImage(imgFile: File) {
-    this.profileService.changeProfileImg(imgFile, this._id).subscribe({
-      next: (res: any) => {
-        this.getProfile();
-      },
-      error: (error: any) => {
-        console.log(error);
-        if (error.status === 413) {
-          this.getProfile();
-          this.dialogService.openDialogNegative(
-            'The file size is too large. Must be less than 15M.'
-          );
-        } else {
-          this.dialogService.openDialogNegative('change profile Error');
-        }
-      },
-    });
+    this.profileService
+      .changeProfileImg(imgFile, this.userInfoStore()._id)
+      .subscribe({
+        next: (res) => {
+          this.refreshProfie();
+        },
+        error: (error) => {
+          console.log(error);
+          if (error.status === 413) {
+            this.router.navigate(['profile']);
+            this.dialogService.openDialogNegative(
+              'The file size is too large. Must be less than 15M.'
+            );
+          } else {
+            this.dialogService.openDialogNegative('An error has occured.');
+          }
+        },
+      });
   }
 
-  onSubmit() {
-    console.log(this.editProfileForm.errors?.['isNotMatched']);
+  updateProfile() {
     if (this.editProfileForm.valid) {
       if (!this.editProfileForm.errors?.['isNotMatched']) {
         const patchData = {
           ...this.editProfileForm.value,
-          _id: this._id,
+          _id: this.userInfoStore()._id,
         };
 
         // updateForm 중 값이 ''이면 객체에서 삭제. patch 시 변경될 값만 설정
@@ -138,22 +108,36 @@ export class ProfileComponent {
           }
         }
 
-        console.log(patchData);
-
         this.profileService.updateProfile(patchData).subscribe({
-          next: (res: any) => {
-            if (res.success) {
-              this.getProfile();
-              this.dialogService.openDialogPositive(
-                'Successfully, Profile has been updated'
-              );
-            }
+          next: (res) => {
+            console.log(res);
+            this.refreshProfie();
+            this.dialogService.openDialogPositive(
+              'Successfully updated profile.'
+            );
+            this.resetPwdInit();
           },
-          error: (error: any) => {
+          error: (error) => {
             console.log(error);
+            this.resetPwdInit();
           },
         });
       }
     }
+  }
+
+  refreshProfie() {
+    const data = {
+      email: this.userInfoStore().email,
+    };
+    this.authService.refreshToken(data).subscribe();
+  }
+
+  resetPwdInit() {
+    this.editProfileForm.reset({
+      name: this.editProfileForm.value.name,
+      password: null,
+      confirmPassword: null,
+    });
   }
 }
--- src/app/services/profile/profile.service.ts
+++ src/app/services/profile/profile.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { HttpResMsg } from '../../interfaces/http-response.interfac';
 import { Observable } from 'rxjs';
+import { HttpResMsg } from '../../interfaces/http-response.interfac';
 
 @Injectable({
   providedIn: 'root',
@@ -10,31 +10,24 @@ import { Observable } from 'rxjs';
 export class ProfileService {
   private baseUrl = environment.apiUrl;
 
-  private http = inject(HttpClient);
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  // 프로필 정보 가져오기
-  getUserProfile() {
-    return this.http.get(this.baseUrl + '/admin/profile');
-  }
-
-  // 프로필 이미지 변경하기
   changeProfileImg(imgae: File, id: string): Observable<HttpResMsg<any>> {
-    console.log(imgae);
-    console.log(id);
     const formData: FormData = new FormData();
     formData.append('file', imgae, imgae?.name);
     formData.append('id', id);
     return this.http.post<HttpResMsg<any>>(
-      this.baseUrl + '/admin/profileImageChange',
-      formData
+      this.baseUrl + '/admin/profiles',
+      formData,
+      {
+        reportProgress: true,
+      }
     );
   }
 
-  // 프로필 정보 업데이트하기
   updateProfile(formData: any): Observable<HttpResMsg<any>> {
     return this.http.patch<HttpResMsg<any>>(
-      this.baseUrl + '/admin/profileChange',
+      this.baseUrl + '/admin/profiles',
       formData
     );
   }
