4704d72e6b1640becb9c10b2a3cc04e20ead453e
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-07-09 11:18:52 +0900

    [feat] dashboard 추가

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MaterialsModule } from '../../materials/materials.module';
+import { DashboardService } from '../../services/dashboard/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MaterialsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  dashboardService = inject(DashboardService);
+
+  date = new Date();
+  companyEmployee = 0;
+  companyRetiredEmployee = 0;
+  employmentCountractRequest = 0;
+  holiday = 0;
+
+  ngAfterViewInit() {
+    this.getDashboard();
+  }
+
+  getDashboard() {
+    this.dashboardService.getDashboard().subscribe(
+      (data: any) => {
+        this.companyEmployee = data.countCompanyEmployee;
+        this.companyRetiredEmployee = data.countCompanyRetiredEmployee;
+        this.employmentCountractRequest = data.countEmploymentCountractRequest;
+        this.holiday = data.countHoliday;
+      },
+      (err: any) => {
+        console.log(err.error);
+      }
+    );
+  }
+}
---
+++ src/app/services/dashboard/dashboard.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DashboardService {
+  private baseUrl = environment.apiUrl;
+
+  private http = inject(HttpClient);
+  constructor() {}
+
+  // 대쉬보드 목록
+  getDashboard() {
+    return this.http.get(this.baseUrl + '/admin/dashboard');
+  }
+}
